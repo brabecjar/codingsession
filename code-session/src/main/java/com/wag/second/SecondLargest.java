1580c5fcce4b6555f37afdbd0f746c4a1bfc01b9
Jaroslav Brabec <brabec.jar@gmail.com>
2023-04-13 16:03:24 +0200

    Redefine SecondLargest

--- code-session/src/main/java/com/wag/second/SecondLargest.java
+++ code-session/src/main/java/com/wag/second/SecondLargest.java
@@ -1,14 +1,19 @@
 package com.wag.second;
 
+import java.util.List;
+
 public class SecondLargest {
 
     /**
-     * You are given a list of integers, and you need to implement a function that returns the second largest number in the list. If the list has less than two elements, return -1.
-     * - You may assume that the input list contains only integers.
-     * - You may not use any built-in sorting functions.
-     * - You should aim for an efficient algorithm that runs in O(n) time complexity.
+     * You are given a list of integers, and you need to implement a function that returns the second-largest number in the list.
+     * Validate that list has at least two elements.
+     * <ul>
+     * <li>You may assume that the input list contains only integers.</li>
+     * <li>You may not use any built-in sorting functions.</li>
+     * <li>You should aim for an efficient algorithm that runs in O(n) time complexity..</li>
+     * </ul>
      */
-    public static int findSecondLargest(int[] arr){
+    public static int findSecondLargest(List<Integer> integers) {
         //TODO Your implementation
         return 0;
     }
--- code-session/src/test/java/com/wag/second/SecondLargestTest.java
+++ code-session/src/test/java/com/wag/second/SecondLargestTest.java
@@ -2,41 +2,33 @@ package com.wag.second;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SecondLargestTest {
     @Test
     public void testFindSecondLargestWithValidInput() {
-        int[] arr = {1, 2, 3, 4, 5};
-        assertEquals(4, SecondLargest.findSecondLargest(arr));
+        List<Integer> integers = List.of(1, 2, 3, 4, 5);
+        assertEquals(4, SecondLargest.findSecondLargest(integers));
     }
 
     @Test
     public void testFindSecondLargestWithDuplicates() {
-        int[] arr = {1, 2, 3, 4, 5, 5, 5};
-        assertEquals(4, SecondLargest.findSecondLargest(arr));
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 5, 5);
+        assertEquals(4, SecondLargest.findSecondLargest(integers));
     }
 
     @Test
-    public void testFindSecondLargestWithNegativeNumbers() {
-        int[] arr = {-5, -2, -1, -10};
-        assertEquals(-2, SecondLargest.findSecondLargest(arr));
+    public void testFindSecondLargestWithTwoElementArray() {
+        List<Integer> integers = List.of(5, 6);
+        assertEquals(5, SecondLargest.findSecondLargest(integers));
     }
 
     @Test
-    public void testFindSecondLargestWithEmptyArray() {
-        int[] arr = {};
-        assertThrows(IllegalArgumentException.class, () -> {
-            SecondLargest.findSecondLargest(arr);
-        });
+    public void testFindSecondLargestWithTwoElementArray02() {
+        List<Integer> integers = List.of(6, 5);
+        assertEquals(5, SecondLargest.findSecondLargest(integers));
     }
 
-    @Test
-    public void testFindSecondLargestWithOneElementArray() {
-        int[] arr = {5};
-        assertThrows(IllegalArgumentException.class, () -> {
-            SecondLargest.findSecondLargest(arr);
-        });
-    }
 }
